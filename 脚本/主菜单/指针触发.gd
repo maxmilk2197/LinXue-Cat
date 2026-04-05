728f475ae329f4d55862ac2d54d547b726654f11
maxmilk2197 <nahida1129@outlook.com>
2026-04-05 17:15:06 +0800

    -

--- 脚本/主菜单/指针触发.gd
+++ 脚本/主菜单/指针触发.gd
@@ -5,12 +5,21 @@ var center_global : Vector2
 func _on_开始_button_down() -> void:
 	center_global = $"../主界面/开始".global_position + $"../主界面/开始".size / 2
 	开始移动()
+func _on_开始_button_up() -> void:
+	center_global = $"../主界面/开始".global_position + $"../主界面/开始".size / 2
+	开始移动()
 
 func _on_设置_button_down() -> void:
 	center_global = $"../主界面/设置".global_position + $"../主界面/设置".size / 2
 	开始移动()
+func _on_设置_button_up() -> void:
+	center_global = $"../主界面/设置".global_position + $"../主界面/设置".size / 2
+	开始移动()
 
 
+func _on_退出_button_up() -> void:
+	center_global = $"../主界面/退出".global_position + $"../主界面/退出".size / 2
+	开始移动()
 func _on_退出_button_down() -> void:
 	center_global = $"../主界面/退出".global_position + $"../主界面/退出".size / 2
 	开始移动()
@@ -18,15 +27,3 @@ func _on_退出_button_down() -> void:
 
 func 开始移动 ():
 	print("移动指针-中心点：", center_global)
-
-
-func _on_开始_button_up() -> void:
-	pass # Replace with function body.
-
-
-func _on_设置_button_up() -> void:
-	pass # Replace with function body.
-
-
-func _on_退出_button_up() -> void:
-	pass # Replace with function body.
--- 脚本/主菜单/选择指针.gd
+++ 脚本/主菜单/选择指针.gd
@@ -9,20 +9,13 @@ enum State {
 
 var current_state = State.开始游戏
 
-func change_state(new_state):
-	current_state = new_state
-	state_changed.emit(new_state)
+# 移动指针到指定全局位置，并可选改变状态
+func move_to(target_global_pos: Vector2, new_state: State = current_state):
+	if new_state != current_state:
+		current_state = new_state
+		state_changed.emit(new_state)
 	
 	var tween = create_tween()
-	var target_pos: Vector2   # 在 match 前声明变量
-	
-	match new_state:
-		State.开始游戏:
-			target_pos = Vector2(240, 240)
-		State.设置:
-			target_pos = Vector2(240, 240)
-		State.更多设置:
-			target_pos = Vector2(1200, 240)
-	tween.tween_property(self, "position", target_pos, 0.8)\
+	tween.tween_property(self, "global_position", target_global_pos, 0.8)\
 		 .set_trans(Tween.TRANS_QUINT)\
 		 .set_ease(Tween.EASE_OUT)
