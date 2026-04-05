a9c936adbf2d4dfac2261d0ed4ece3319240f635
maxmilk2197 <nahida1129@outlook.com>
2026-04-05 23:44:31 +0800

    -

--- 脚本/主菜单/九宫格.gd
+++ 脚本/主菜单/九宫格.gd
@@ -2,14 +2,17 @@ extends NinePatchRect
 
 @onready var camera = get_node("../Camera2D")
 
-
-func _on_退出_pressed() -> void:
-	camera.change_state(camera.State.退出)
-
-
+func _on_开始_pressed() -> void:
+	camera.change_state(camera.State.游戏存档)
+func _on_开始_button_up() -> void:
+	camera.change_state(camera.State.游戏存档)
+	
+func _on_设置_button_up() -> void:
+	camera.change_state(camera.State.设置)
 func _on_设置_pressed() -> void:
 	camera.change_state(camera.State.设置)
 
-
-func _on_开始_pressed() -> void:
-	camera.change_state(camera.State.游戏存档)
+func _on_退出_button_up() -> void:
+	camera.change_state(camera.State.退出)
+func _on_退出_pressed() -> void:
+	camera.change_state(camera.State.退出)
