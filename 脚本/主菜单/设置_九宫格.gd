34f50842ae2bedc47e3b3e271aadb1c5862330e1
maxmilk2197 <nahida1129@outlook.com>
2026-04-05 17:36:03 +0800

    -

--- 脚本/主菜单/设置_九宫格.gd
+++ 脚本/主菜单/设置_九宫格.gd
@@ -4,11 +4,13 @@ func _ready() -> void:
 	$"../../更多设置/更多设置_九宫格_音量".hide()
 	$"../../更多设置/更多设置_九宫格_AI or Link".hide()
 	$"../../更多设置/更多设置_九宫格_关于".hide()
+	$"../../更多设置/测试选项".hide()
 
 func _on_音量设置_pressed() -> void:
 	$"../../更多设置/更多设置_九宫格_音量".show()
 	$"../../更多设置/更多设置_九宫格_AI or Link".hide()
 	$"../../更多设置/更多设置_九宫格_关于".hide()
+	$"../../更多设置/测试选项".hide()
 	camera.change_state(camera.State.更多设置)
 
 
@@ -16,6 +18,7 @@ func _on_ai_or_link_设置_pressed() -> void:
 	$"../../更多设置/更多设置_九宫格_音量".hide()
 	$"../../更多设置/更多设置_九宫格_AI or Link".show()
 	$"../../更多设置/更多设置_九宫格_关于".hide()
+	$"../../更多设置/测试选项".hide()
 	camera.change_state(camera.State.更多设置)
 
 
@@ -23,4 +26,13 @@ func _on_关于_pressed() -> void:
 	$"../../更多设置/更多设置_九宫格_音量".hide()
 	$"../../更多设置/更多设置_九宫格_AI or Link".hide()
 	$"../../更多设置/更多设置_九宫格_关于".show()
+	$"../../更多设置/测试选项".hide()
 	camera.change_state(camera.State.更多设置)
+
+
+func _on_测试选项_pressed() -> void:
+	$"../../更多设置/更多设置_九宫格_音量".hide()
+	$"../../更多设置/更多设置_九宫格_AI or Link".hide()
+	$"../../更多设置/更多设置_九宫格_关于".hide()
+	$"../../更多设置/测试选项".show()
+	camera.change_state(camera.State.更多设置) 
